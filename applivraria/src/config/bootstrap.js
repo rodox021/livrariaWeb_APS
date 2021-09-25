6f5afd0e74c8c564831c2354707670b913d1ecee
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2021-09-24 21:43:11 -0300

    del
    l

--- applivraria/src/config/bootstrap.js
+++
@@ -1,9 +0,0 @@
-
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
--- applivraria/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">
-   HOME
-  </div>
-</template>
-
-<script>
-
-</script>
